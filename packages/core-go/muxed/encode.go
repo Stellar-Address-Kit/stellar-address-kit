f5ddfb535aab4e799d76d0bec4ef2e23f4fc3e4a
David Ojo <victorojodavid@gmail.com>
2026-02-24 18:54:11 +0100

    solved

--- packages/core-go/muxed/encode.go
+++ packages/core-go/muxed/encode.go
@@ -1,18 +1,30 @@
 package muxed
 
 import (
-	"math/big"
+	"encoding/binary"
+	"errors"
+	"strings"
+
 	"github.com/stellar/go/strkey"
 )
 
-func EncodeMuxed(baseG string, id string) (string, error) {
+func EncodeMuxed(baseG string, id uint64) (string, error) {
+	if len(baseG) != 56 {
+		return "", errors.New("invalid G address: must be 56 characters long")
+	}
+	
+	if !strings.HasPrefix(baseG, "G") {
+		return "", errors.New("invalid G address: must start with G")
+	}
+	
 	pubkey, err := strkey.Decode(strkey.VersionByteAccountID, baseG)
 	if err != nil {
 		return "", err
 	}
 	
-	idInt := new(big.Int)
-	idInt.SetString(id, 10)
+	payload := make([]byte, 40)
+	copy(payload[0:32], pubkey)
+	binary.BigEndian.PutUint64(payload[32:40], id)
 	
-	return strkey.EncodeMuxedAccount(pubkey, idInt.Uint64())
+	return strkey.Encode(strkey.VersionByteMuxedAccount, payload)
 }
